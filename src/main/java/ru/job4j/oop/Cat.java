27db27984405d001dfb8d590104ab7671f3f31f9
Anatoly-D <dav119@mail.ru>
2020-07-16 21:54:48 +0300

    1.5. Поля объекта [#324911]

--- src/main/java/ru/job4j/oop/Cat.java
+++ src/main/java/ru/job4j/oop/Cat.java
@@ -1,12 +1,40 @@
 package ru.job4j.oop;
 
 public class Cat {
+    private String food;
+    private String name;
+
     public static void main(String[] args) {
         Cat peppy = new Cat();
         Cat sparky = new Cat();
 
         String say = peppy.sound();
         System.out.println("Peppy says: " + say);
+
+        Cat gav = new Cat();
+        gav.eat("kotleta");
+        gav.giveName("Gav");
+        System.out.println("What's a favorite Gav's food?");
+        gav.show();
+
+        Cat black = new Cat();
+        black.eat("fish");
+        black.giveName("Black");
+        System.out.println("What's a favorite Black's food?");
+        black.show();
+
+    }
+
+    private void giveName(String name) {
+        this.name = name;
+    }
+
+    private void show() {
+        System.out.println(this.name + " ate " + this.food);
+    }
+
+    private void eat(String favoriteFood) {
+        this.food = favoriteFood;
     }
 
     private String sound() {
