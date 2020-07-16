da43754ffbfe0b1125687ed85b9734ce80fecda3
Anatoly-D <dav119@mail.ru>
2020-07-16 22:10:07 +0300

    1.7. Состояние объекта [#324916]

---
+++ src/main/java/ru/job4j/oop/Battery.java
@@ -0,0 +1,25 @@
+package ru.job4j.oop;
+
+public class Battery {
+    private int capacity;
+
+    public Battery(int capacity) {
+        this.capacity = capacity;
+    }
+
+    public void exchange(Battery another) {
+        another.capacity += this.capacity;
+        this.capacity = 0;
+    }
+
+    public static void main(String[] args) {
+        Battery first = new Battery(10);
+        Battery second = new Battery(12);
+
+        System.out.println("Initial state. First: " + first.capacity + " Second: " + second.capacity);
+        first.exchange(second);
+        System.out.println("After exchange. First: " + first.capacity + " Second: " + second.capacity);
+    }
+
+
+}
