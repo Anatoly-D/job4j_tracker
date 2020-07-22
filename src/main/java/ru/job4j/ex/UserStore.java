5c6a2891f0634d10a8479c06509b20081206badc
Anatoly-D <dav119@mail.ru>
2020-07-22 08:36:55 +0300

    0.5. Иерархия исключений и множественный catch. [#324898]

--- src/main/java/ru/job4j/ex/UserStore.java
+++ src/main/java/ru/job4j/ex/UserStore.java
@@ -8,31 +8,33 @@ public class UserStore {
                 return user;
             }
         }
-        throw new UserNotFoundException();
+        throw new UserNotFoundException("User not found mine.");
     }
 
     public static boolean validate(User user) throws UserInvalidException {
         if (!user.isValid() || user.getUsername().length() < 3) {
-            throw new UserInvalidException();
+            throw new UserInvalidException("User invalid mine");
         }
         return true;
     }
 
     public static void main(String[] args) {
         User[] users = {
-                new User("Petr Arsentev", true),
+                new User("Petr Arsentev", false),
                 new User("Ivan Ivanov", false),
                 new User("Sidor Sidorov", true)
         };
         try {
-            User user = findUser(users, "Petr Arsentev");
+            User user = findUser(users, "Petr Artsentev");
             if (validate(user)) {
                 System.out.println("This user has an access");
             }
         } catch (UserInvalidException invalidException) {
             System.out.println("The user is invalid.");
+            invalidException.printStackTrace();
         } catch (UserNotFoundException notFoundException) {
             System.out.println("The user was not found");
+            notFoundException.printStackTrace();
         }
     }
 }
