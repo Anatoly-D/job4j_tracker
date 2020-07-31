8e49bed4fa983ef9caae7ec09cb2b5916649f55c
Anatoly-D <dav119@mail.ru>
2020-07-31 11:07:02 +0300

    6. Изменить программу Tracker из 2-го модуля [#324859]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -14,9 +14,9 @@ public class Tracker {
     }
 
     private int indexOf(int id) {
-        for (Item item : items) {
-            if (item.getId() == id) {
-                return items.indexOf(item);
+        for (int index = 0; index < items.size(); index++) {
+            if (items.get(index).getId() == id) {
+                return index;
             }
         }
         return -1;
