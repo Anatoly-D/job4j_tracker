ed281a8ea15ee3f73fe777b7efb1e250cc6c22a2
Anatoly-D <dav119@mail.ru>
2020-08-02 10:05:21 +0300

    4. Сортировка номера [#324849]

--- src/main/java/ru/job4j/collections/LexSort.java
+++ src/main/java/ru/job4j/collections/LexSort.java
@@ -5,22 +5,9 @@ import java.util.Comparator;
 public class LexSort implements Comparator<String> {
     @Override
     public int compare(String left, String right) {
-        String[] leftArr = left.split(" ");
-        String[] rightArr = right.split(" ");
-        leftArr = leftArr[0].split("\\.");
-        rightArr = rightArr[0].split("\\.");
+        String[] leftArr = left.split("\\. ");
+        String[] rightArr = right.split("\\. ");
 
-        int minLength = leftArr.length < rightArr.length ? leftArr.length : rightArr.length;
-        int leftNumber;
-        int rightNumber;
-
-        for (int i = 0; i < minLength; i++) {
-            leftNumber = Integer.valueOf(leftArr[i]);
-            rightNumber = Integer.valueOf(rightArr[i]);
-            if (leftNumber != rightNumber) {
-                return leftNumber - rightNumber;
-            }
-        }
-        return leftArr.length - rightArr.length;
+        return Integer.compare(Integer.valueOf(leftArr[0]), Integer.valueOf(rightArr[0]));
     }
 }
