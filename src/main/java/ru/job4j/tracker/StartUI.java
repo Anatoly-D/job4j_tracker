a64d984ac00ac947cf8c90c55eace351cb3f1fe2
Anatoly-D <dav119@mail.ru>
2023-09-25 11:39:11 +0300

    3.1. Реализация класса StartUI. Добавление заявки [#500744]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -11,9 +11,14 @@ public class StartUI {
             showMenu();
             System.out.print("Select: ");
             int select = Integer.parseInt(scanner.nextLine());
-            if (select != 6) {
-                System.out.println("Пользователь выбрал: " + select);
-            } else {
+            if (select == 0) {
+                System.out.println("=== Create a new Item ===");
+                System.out.print("Enter Name: ");
+                String name = scanner.nextLine();
+                Item newItem = new Item(name);
+                tracker.add(newItem);
+                System.out.println("New Item is created: " + newItem.getName());
+            } else if (select == 6) {
                 run = false;
             }
         }
@@ -25,6 +30,7 @@ public class StartUI {
                 "Delete item", "Find item by id", "Find items by name",
                 "Exit Program"
         };
+
         System.out.println("Menu:");
         for (int i = 0; i < menu.length; i++) {
             System.out.println(i + ". " + menu[i]);
