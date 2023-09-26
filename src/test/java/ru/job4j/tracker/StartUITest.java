fdc7c72f01def9d888ca6994071fac8411f6d4f2
Anatoly-D <dav119@mail.ru>
2023-09-26 19:54:16 +0300

    12.0. Написать тесты на StartUI. [#785 #144395]

--- src/test/java/ru/job4j/tracker/StartUITest.java
+++ src/test/java/ru/job4j/tracker/StartUITest.java
@@ -4,5 +4,51 @@ import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class StartUITest {
+    @Test
+    public void whenCreateItem() {
+        Input in = new StubInput(
+                new String[] {"0", "Item name", "1"}
+        );
+        Tracker tracker = new Tracker();
+        UserAction[] actions = {
+                new CreateAction(),
+                new ExitAction()
+        };
+        new StartUI().init(in, tracker, actions);
+        assertThat(tracker.findAll()[0].getName()).isEqualTo("Item name");
+    }
+
+    @Test
+    public void whenReplaceItem() {
+        Tracker tracker = new Tracker();
+        Item item = tracker.add(new Item("Old item name")); /* Добавляется в tracker новая заявка */
+        String replacedName = "New item name";
+        Input in = new StubInput(
+                new String[] {"0", "1", "1", "New item name", "0", "2"}
+        );
+        UserAction[] actions = {
+                new ShowAllItemsAction(),
+                new ReplaceItemAction(),
+                new ExitAction()
+        };
+        new StartUI().init(in, tracker, actions);
+        assertThat(tracker.findById(item.getId()).getName()).isEqualTo(replacedName);
+    }
+
+    @Test
+    public void whenDeleteItem() {
+        Tracker tracker = new Tracker();
+        Item item = tracker.add(new Item("Deleted item")); /* Добавляется в tracker новая заявка */
+        Input in = new StubInput(
+                new String[] {"0", "1", "1", "0", "2"}
+        );
+        UserAction[] actions = {
+                new ShowAllItemsAction(),
+                new DeleteItemAction(),
+                new ExitAction()
+        };
+        new StartUI().init(in, tracker, actions);
+        assertThat(tracker.findById(item.getId())).isNull();
+    }
 
 }
