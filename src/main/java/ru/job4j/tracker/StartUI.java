56e3b788da8271ff1ae320723925fb9dfe4942e5
Anatoly-D <dav119@mail.ru>
2023-09-28 20:24:09 +0300

    1. Обеспечить бесперебойную работу приложения Tracker [#789 #144406]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -12,6 +12,10 @@ public class StartUI {
         while (run) {
             this.showMenu(userActions);
             int select = input.askInt("Select: ");
+            if (select < 0 || select >= userActions.length) {
+                output.println("Wrong input, you can select: 0 .. " + (userActions.length - 1));
+                continue;
+            }
             UserAction action = userActions[select];
             run = action.execute(input, tracker);
         }
@@ -26,7 +30,7 @@ public class StartUI {
 
     public static void main(String[] args) {
         Output output = new ConsoleOutput();
-        Input input = new ConsoleInput();
+        Input input = new ValidateInput();
         Tracker tracker = new Tracker();
         UserAction[] userActions = new UserAction[]{
                 new CreateAction(output),
