d60924521fd91b970807e8aec80ec28db04bb50b
Anatoly-D <dav119@mail.ru>
2023-09-29 13:08:18 +0300

    4. Принципы раннего возврата и охранных выражений [#504875 #448709]

--- src/main/java/early/PasswordValidator.java
+++ src/main/java/early/PasswordValidator.java
@@ -3,25 +3,6 @@ package early;
 public class PasswordValidator {
     private static final String[] FORBIDDEN = {"qwerty", "12345", "password", "admin", "user"};
 
-    /**
-     * Метод проверят пароль по правилам:
-     *  1. Если password null, то выбросить исключение "Password can't be null";
-     *  2. Длина пароля находится в диапазоне [8, 32],
-     *     если нет то "Password should be length [8, 32]";
-     *  3. Пароль содержит хотя бы один символ в верхнем регистре,
-     *     если нет то "Password should contain at least one uppercase letter");
-     *  4. Пароль содержит хотя бы один символ в нижнем регистре,
-     *     если нет то "Password should contain at least one lowercase letter";
-     *  5. Пароль содержит хотя бы одну цифру,
-     *     если нет то"Password should contain at least one figure");
-     *  6. Пароль содержит хотя бы один спец. символ (не цифра и не буква),
-     *     если нет то "Password should contain at least one special symbol");
-     *  7. Пароль не содержит подстрок без учета регистра: qwerty, 12345, password, admin, user.
-     *     Без учета регистра, значит что, например, ни qwerty ни QWERTY ни qwErty и т.п.
-     *     если да, то "Password shouldn't contain substrings: qwerty, 12345, password, admin, user".
-     * @param password Пароль
-     * @return Вернет пароль или выбросит исключение.
-     */
     public static String validate(String password) {
         if (password == null) {
             throw new IllegalArgumentException("Password can't be null");
@@ -31,17 +12,13 @@ public class PasswordValidator {
         boolean hasLowCase = false;
         boolean hasDigit = false;
         boolean hasSpecial = false;
-        boolean hasCorrectLength = false;
-        boolean hasForbidden = false;
-        int pwdLength = password.length();
         String pwdLowerCase = password.toLowerCase();
 
-        if (pwdLength >= 8 && pwdLength <= 32) {
-            hasCorrectLength = true;
+        if (password.length() < 8 || password.length() > 32) {
+            throw new IllegalArgumentException("Password should be length [8, 32]");
         }
 
         for (char symbol : password.toCharArray()) {
-            /* Блок проверки принадлежности символа к определенной группе - Character.is ... */
             if (Character.isUpperCase(symbol)) {
                 hasUpCase = true;
             }
@@ -54,40 +31,28 @@ public class PasswordValidator {
             if (!Character.isLetter(symbol) && !Character.isDigit(symbol)) {
                 hasSpecial = true;
             }
-        }
-
-        for (String string : FORBIDDEN) {
-            if (pwdLowerCase.contains(string)) {
-                hasForbidden = true;
+            if (hasUpCase && hasLowCase && hasDigit && hasSpecial) {
                 break;
             }
         }
 
+        for (String string : FORBIDDEN) {
+            if (pwdLowerCase.contains(string)) {
+                throw new IllegalArgumentException("Password shouldn't contain substrings: qwerty, 12345, password, admin, user");
+            }
+        }
+
         if (!hasUpCase) {
-            throw new IllegalArgumentException(
-                    "Password should contain at least one uppercase letter"
-            );
+            throw new IllegalArgumentException("Password should contain at least one uppercase letter");
         }
         if (!hasLowCase) {
-            throw new IllegalArgumentException(
-                    "Password should contain at least one lowercase letter"
-            );
+            throw new IllegalArgumentException("Password should contain at least one lowercase letter");
         }
         if (!hasDigit) {
-            throw new IllegalArgumentException(
-                    "Password should contain at least one figure"
-            );
+            throw new IllegalArgumentException("Password should contain at least one figure");
         }
         if (!hasSpecial) {
-            throw new IllegalArgumentException(
-                    "Password should contain at least one special symbol"
-            );
-        }
-        if (!hasCorrectLength) {
-            throw new IllegalArgumentException("Password should be length [8, 32]");
-        }
-        if (hasForbidden) {
-            throw new IllegalArgumentException("Password shouldn't contain substrings: qwerty, 12345, password, admin, user");
+            throw new IllegalArgumentException("Password should contain at least one special symbol");
         }
         return password;
     }
