5fecb9c34d4c9afb4b0ca0707332da1a6494697d
Anatoly-D <dav119@mail.ru>
2023-09-30 08:10:28 +0300

    3. final [#334073 #448737]

--- src/main/java/ooa/Airbus.java
+++ src/main/java/ooa/Airbus.java
@@ -23,7 +23,7 @@ public final class Airbus extends Aircraft {
     }
 
     public void printCountEngine() {
-        System.out.println("Количество двигателей равно: " + (getName().equals("A380") ? 4 : COUNT_ENGINE));
+        System.out.println("Количество двигателей равно: " + (("A380").equals(getName()) ? 4 : COUNT_ENGINE));
     }
 
     @Override
