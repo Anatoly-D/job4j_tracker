63fde67ae350ee46a73f69a203bcb07b4f440b8d
Anatoly-D <dav119@mail.ru>
2023-10-03 16:35:52 +0300

    1. Паспорт и жители [#212656 #144375]

---
+++ src/test/java/collection/PassportOfficeTest.java
@@ -0,0 +1,17 @@
+package collection;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+class PassportOfficeTest {
+    @Test
+    public void whenTestAddMethod() {
+        Citizen citizen = new Citizen("2f44a", "Petr Arsentev");
+        PassportOffice office = new PassportOffice();
+        office.add(citizen);
+        assertThat(office.get(citizen.getPassport())).isEqualTo(citizen);
+    }
+
+}
