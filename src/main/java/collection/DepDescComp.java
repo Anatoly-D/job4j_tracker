74a3591ca0f2193a5d9743c826682aef471e9411
Anatoly-D <dav119@mail.ru>
2023-10-08 16:08:48 +0300

    3. Отсортировать департаменты [#34352 #450840]

--- src/main/java/collection/DepDescComp.java
+++ src/main/java/collection/DepDescComp.java
@@ -9,13 +9,6 @@ public class DepDescComp implements Comparator<String> {
         String[] o1Split = o1.split("/", 2);
         String[] o2Split = o2.split("/", 2);
         result = o2Split[0].compareTo(o1Split[0]);
-        if (result == 0) {
-            if (o1Split.length > 1 && o2Split.length > 1) {
-                result = o1Split[1].compareTo(o2Split[1]);
-            } else {
-                result = o1Split.length - o2Split.length;
-            }
-        }
-        return result;
+        return result != 0 ? result : o1.compareTo(o2);
     }
 }
