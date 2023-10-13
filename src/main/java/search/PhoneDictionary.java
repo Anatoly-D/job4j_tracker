33ea0c49c7307270d04902586ce8ec49bbfe9b5c
Anatoly-D <dav119@mail.ru>
2023-10-13 21:05:05 +0300

    7. Функции высшего порядка [#24255]

---
+++ src/main/java/search/PhoneDictionary.java
@@ -0,0 +1,27 @@
+package search;
+
+import java.util.ArrayList;
+import java.util.function.Predicate;
+
+public class PhoneDictionary {
+    private ArrayList<Person> persons = new ArrayList<>();
+
+    public void add(Person person) {
+        this.persons.add(person);
+    }
+
+    public ArrayList<Person> find(String key) {
+        Predicate<Person> checkName = person -> person.getName().equals(key);
+        Predicate<Person> checkSurname = person -> person.getSurname().equals(key);
+        Predicate<Person> checkPhone = person -> person.getPhone().equals(key);
+        Predicate<Person> checkAddress = person -> person.getAddress().equals(key);
+        Predicate<Person> combine = checkName.or(checkSurname.or(checkPhone.or(checkAddress)));
+        ArrayList<Person> result = new ArrayList<>();
+        for (Person person : persons) {
+            if (combine.test(person)) {
+                result.add(person);
+            }
+        }
+        return result;
+    }
+}
