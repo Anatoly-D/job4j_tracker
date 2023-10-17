04a63de317d18f1e4c53f7bbd4836973915d2db9
Anatoly-D <dav119@mail.ru>
2023-10-17 08:48:44 +0300

    10. Преобразование матрицы чисел в список чисел. FlatMap. [#110228]

---
+++ src/main/java/stream/MatrixToList.java
@@ -0,0 +1,14 @@
+package stream;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class MatrixToList {
+
+    public static List<Integer> convert(Integer[][] matrix) {
+        return Stream.of(matrix)
+                .flatMap(Stream::of)
+                .collect(Collectors.toList());
+    }
+}
